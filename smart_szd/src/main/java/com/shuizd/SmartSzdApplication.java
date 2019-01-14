605b26ea7e73c2e674e199595bd5444bf8de1d91
Yaco <luyou0401@163.com>
2019-01-14 17:04:36 +0800

    test sts git

---
+++ smart_szd/src/main/java/com/shuizd/SmartSzdApplication.java
@@ -0,0 +1,14 @@
+package com.shuizd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSzdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSzdApplication.class, args);
+	}
+
+}
+
